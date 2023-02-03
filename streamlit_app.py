37552da3e1830c189e0b879e0d7427e81f23c3e7
AndyPaPaLeu <46556065+AndyPaPaLeu@users.noreply.github.com>
2023-02-03 01:21:24 -0700

    Update streamlit_app.py
    modify

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #  Second Text Entry Box
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
